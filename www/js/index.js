fcb6ebc399f148f76beb88694998b7d62ed64bfa
rothattack <rothattack@gmail.com>
2014-10-13 15:00:07 -0500

    Initial Commit

---
+++ www/js/index.js
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        $(window).on('load', this.fadeOverlay);
+        $('.camera').click(this.takePhoto);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+    fadeOverlay: function() {
+        setTimeout(function() {
+            $('.overlay').fadeOut(800);
+        }, 800);
+    },
+
+    takePhoto: function (e) {
+        e.preventDefault();
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 100,
+            destinationType: Camera.DestinationType.DATA_URL
+         }); 
+
+        function onSuccess(imageData) {
+            var image = document.querySelector('.load-image');
+            image.src = "data:image/jpeg;base64," + imageData;
+            alert(image.src);
+            $('body').css({ 'background-image': image.src });
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+    }
+};
